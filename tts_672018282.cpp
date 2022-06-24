733d0bb9543e7dbcfc499604bad25ae0a2908074
vins <sumbaluwudevin@gmail.com>
2022-06-24 13:16:20 +0700

    add wall

--- tts_672018282.cpp
+++ tts_672018282.cpp
@@ -1,4 +1,6 @@
 #include<GL/freeglut.h>
+
+
 //Deklarasi fungsi Mouse agar gambar 3d dapat diputar putar menggunakan Mouse
 float xrot =0;
 float yrot = 0;
@@ -6,17 +8,21 @@ float xdiff = 0;
 float ydiff = 0;
 bool mouseDown = false;
 
+
+
 //Deklarasi pengaturan lembaran kerja agar Gambar 3d yang kita buat saat diputar atau di geser tidak kemana mana
 void ukur(int lebar, int tinggi){
-if(tinggi==0) tinggi=1;
-glMatrixMode(GL_PROJECTION);
-glLoadIdentity();
-gluPerspective(45,lebar/tinggi, 5, 450);
-glTranslatef(0,0,-300);// jarak object dari lembaran kerja
-glMatrixMode(GL_MODELVIEW);
+    if(tinggi==0) tinggi=1;
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(45,lebar/tinggi, 5, 450);
+    glTranslatef(0,0,-300);// jarak object dari lembaran kerja
+    glMatrixMode(GL_MODELVIEW);
 }
+
+
 void myinit(void){
-    glClearColor (0.0, 0.0, 0.0, 0.0);
+    glClearColor (0.0, 1.0, 0.0, 0.0);
     glMatrixMode(GL_PROJECTION);
     glEnable(GL_DEPTH_TEST);
 
@@ -69,18 +75,22 @@ void tampilan(void){
 
     //LUAS TANAH
     glBegin(GL_POLYGON);
-    glColor3f(1, 0.5, 0);
+    glColor3f(1, 1, 1);
     glVertex3f(100,150,0);
+    glVertex3f(100,130,0);
     glVertex3f(100,-100,0);
     glVertex3f(-100,-100,0);
+    glVertex3f(-100,130,0);
     glVertex3f(-100,150,0);
     glEnd();
 
     //luas gereja
     glBegin(GL_POLYGON);
     glColor3f(0, 0, 1);
-    glVertex3f(-50,-50,0);
-    glVertex3f(50,-50,0);
+    glVertex3f(-50,-40,0);
+    glVertex3f(-30,-50,0);
+    glVertex3f(30,-50,0);
+    glVertex3f(50,-40,0);
     glVertex3f(50,100,0);
     glVertex3f(50,150,0);
     glVertex3f(-50,150,0);
@@ -90,7 +100,7 @@ void tampilan(void){
 
     //gedung kanan
     glBegin(GL_POLYGON);
-    glColor3f(0, 0.8, 0);
+    glColor3f(0.431, 0.431, 0.431);
     glVertex3f(90,150,0);
     glVertex3f(50,150,0);
     glVertex3f(50,100,0);
@@ -99,13 +109,115 @@ void tampilan(void){
 
     //gedung kiri
     glBegin(GL_POLYGON);
-    glColor3f(0, 0.8, 0);
+    glColor3f(0.431, 0.431, 0.431);
     glVertex3f(-50,150,0);
     glVertex3f(-90,150,0);
     glVertex3f(-90,100,0);
     glVertex3f(-50,100,0);
     glEnd();
 
+    //pagar kiri
+    glBegin(GL_POLYGON);
+    glColor3f(0.733, 0.796, 0.807);
+    glVertex3f(-100,-100,0);
+    glVertex3f(-100,-100,10);
+    glVertex3f(-100,130,10);
+    glVertex3f(-100,130,0);
+    glEnd();
+    
+    //pagar kanan
+    glBegin(GL_POLYGON);
+    glColor3f(0.733, 0.796, 0.807);
+    glVertex3f(100,-100,0);
+    glVertex3f(100,-100,10);
+    glVertex3f(100,130,10);
+    glVertex3f(100,130,0);
+    glEnd();
+
+    //pagar pojok kanan1
+    glBegin(GL_POLYGON);
+    glColor3f(0.733, 0.796, 0.807);
+    glVertex3f(100,130,0);
+    glVertex3f(100,130,10);
+    glVertex3f(100,130,20);
+    glVertex3f(100,150,20);
+    glVertex3f(100,150,0);
+    glEnd();
+
+    //pagar pojok kanan2
+    glBegin(GL_POLYGON);
+    glColor3f(0.733, 0.796, 0.807);
+    glVertex3f(100,150,0);
+    glVertex3f(100,150,20);
+    glVertex3f(90,150,20);
+    glVertex3f(90,150,0);
+    glEnd();
+
+    //pagar pojok kiri1
+    glBegin(GL_POLYGON);
+    glColor3f(0.733, 0.796, 0.807);
+    glVertex3f(-100,130,0);
+    glVertex3f(-100,130,10);
+    glVertex3f(-100,130,20);
+    glVertex3f(-100,150,20);
+    glVertex3f(-100,150,0);
+    glEnd();
+
+    //pagar pojok kiri2
+    glBegin(GL_POLYGON);
+    glColor3f(0.733, 0.796, 0.807);
+    glVertex3f(-100,150,0);
+    glVertex3f(-100,150,20);
+    glVertex3f(-90,150,20);
+    glVertex3f(-90,150,0);
+    glEnd();
+
+
+    //dinding gereja kanan
+    glBegin(GL_POLYGON);
+    glColor3f(0.733, 0.796, 0.807);
+    glVertex3f(50,100,0);
+    glVertex3f(50,100,100);
+    glVertex3f(50,-40,100);
+    glVertex3f(50,-40,0);
+    glEnd();
+
+    //dinding gereja kanan2
+    glBegin(GL_POLYGON);
+    glColor3f(0.733, 0.796, 0.807);
+    glVertex3f(50,-40,0);
+    glVertex3f(50,-40,100);
+    glVertex3f(30,-50,100);
+    glVertex3f(30,-50,0);
+    glEnd();
+
+    //dinding gereja kiri
+    glBegin(GL_POLYGON);
+    glColor3f(0.733, 0.796, 0.807);
+    glVertex3f(-50,-40,0);
+    glVertex3f(-50,-40,100);
+    glVertex3f(-50,100,100);
+    glVertex3f(-50,100,0);
+    glEnd();
+
+
+    //dinding gereja kiri2
+    glBegin(GL_POLYGON);
+    glColor3f(0.733, 0.796, 0.807);
+    glVertex3f(-50,-40,0);
+    glVertex3f(-30,-50,0);
+    glVertex3f(-30,-50,100);
+    glVertex3f(-50,-40,100);
+    glEnd();
+
+    //dinding gereja depan
+    glBegin(GL_POLYGON);
+    glColor3f(0.733, 0.796, 0.807);
+    glVertex3f(-30,-50,0);
+    glVertex3f(30,-50,0);
+    glVertex3f(30,-50,100);
+    glVertex3f(-30,-50,100);
+    glEnd();
 
     glPushMatrix();
     glPopMatrix();
